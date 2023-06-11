d9104303e73e344c5b40c8a49b0623aa3a2b293b
Emmanuel Yegon <eyegon@btn-solutions.co.ke>
2023-06-12 00:14:41 +0300

    microservice commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10001,'USD','INR', 84, '');
+
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10002,'KES','USD', 65, '');
+
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10003,'EUR','AUD', 90, '');
